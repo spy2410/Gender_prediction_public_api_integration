71d8413564ff3e4decfe8d8a70096bf91bd00c73
Vatsal Mandaliya <90092362+spy2410@users.noreply.github.com>
2023-01-29 15:56:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+document.getElementById('getApi').addEventListener('click', getApi);
+document.getElementById('getApi').addEventListener('onkeydown', getApi);
+const apiId = 'e2d2fe2c76f848a2b1570500222607';
+
+function getApi() {
+
+  name = encodeURI(document.getElementById('ipAddress').value);
+  console.log("IP address :", name);
+
+  fetch(`https://api.genderize.io?name=${name}`, {
+    method: 'GET',
+  })
+    .then(function (data) {
+      console.log(data);
+      return data.json();
+    })
+    .then((data) => {
+      let output = `
+            <div>
+                <div style="color:black; width:10rem; background-color:white; border-radius: 10px;" class="p-3">        
+                    <p class="m-0" style="font-width:bold; font-size:2rem;">Gender</p>
+                    <p class="m-0" style="font-width:bold; font-size:2rem;">${data.gender.toUpperCase()}</sup></p>
+                </div>
+            </div>
+            `;
+      console.log('printing data', data);
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch((err) => console.log(err))
+}
